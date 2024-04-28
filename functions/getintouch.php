d139d4950b372b172f77ffdd1c3029ca26461110
unknown <beka.imnadze@gmail.com>
2024-04-28 13:15:08 +0400

    fixing get in touch file

--- functions/getintouch.php
+++ functions/getintouch.php
@@ -1,7 +1,7 @@
 <?php 
 
-if($_POST["username"] &&  $_POST["email"] &&  $_POST["phone"] &&  $_POST["message"]){
-    echo 'Your form is submitted.';
+if($_POST["username"] &&  $_POST["email"] &&  $_POST["pone"] &&  $_POST["message"]){
+    echo 'Your form is submetted.';
 }else{
     echo "Please filled in all required fields";
 }
