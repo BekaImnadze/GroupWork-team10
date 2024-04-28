373b7f83544d722c3ce4312b752823742a5b56c0
unknown <beka.imnadze@gmail.com>
2024-04-28 13:10:15 +0400

    fixing appointment file

--- functions/appoitment.php
+++ functions/appoitment.php
@@ -4,20 +4,20 @@
 
 if (
     !empty($_POST["name"]) &&
-    !empty($_POST["phone"]) &&
+    !empty($_POST["pohne"]) &&
     !empty($_POST["date"]) &&
     !empty($_POST["inputSymptoms"]) &&
     !empty($_POST["doctor"])&&
     !empty($_POST["department"])
 ) {
     echo $_POST["name"] . "<br>" .
-         $_POST["phone"] . "<br>" .
+         $_POST["pohne"] . "<br>" .
          $_POST["inputSymptoms"] . "<br>" .
          $_POST["date"] . "<br>" .
          $_POST["doctor"] . "<br>" .
          $_POST["department"]   . "<br>" .
          "Your reservation has been accepted" . "<br>";
 } else {
-    echo "You have not filled all required fields";
+    echo "You have not filled in all required fields";
 }
 
